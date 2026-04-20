338205e8ff9b1a5a8a2c0fca8ec3e49296086b19
Julio Garcia <julio@gepgroup.es>
2026-04-20 16:54:31 +0200

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
-export default defineConfig({
-  site: 'https://gepprotect.netlify.app',
-  integrations: [tailwind({ applyBaseStyles: false })],
-});
